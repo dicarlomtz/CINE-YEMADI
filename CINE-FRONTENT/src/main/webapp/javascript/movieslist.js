02857fbabc1c31423c55fe6be65605e6ca61bcb2
DESKTOP-F2VRT51\mg184 <mauricio.gutierrez.vasquez@est.una.ac.cr>
2021-06-08 17:35:02 -0600

    Primera implementación de la lista de películas

---
+++ CINE-FRONTENT/src/main/webapp/javascript/movieslist.js
@@ -0,0 +1,28 @@
+function init()
+{
+    fetch('MoviesListService').then(function(resultado) {
+        return resultado.json();
+    }).then(function(datos) {
+       cargarCartelera(datos['datos_peliculas']);
+    });
+}
+
+function cargarCartelera(datos)
+{
+    var ref = document.getElementById('movielist');
+    
+    if(ref)
+    {
+        datos.forEach((fila) => {
+            
+            var nuevaFila = ref.insertRow(-1);
+            var nuevaCelda;
+            
+            nuevaCelda = nuevaFila.insertCell(-1);
+            nuevaCelda.textContent = fila.informacion;
+            nuevaCelda.setAttribute('class', 'd1');
+        });
+    }
+}
+
+window.onload = init;
