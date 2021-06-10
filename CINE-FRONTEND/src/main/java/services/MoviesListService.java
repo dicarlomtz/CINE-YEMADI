b41506815edfd2c822d00e1ef57c40e35d1df4e6
DESKTOP-F2VRT51\mg184 <mauricio.gutierrez.vasquez@est.una.ac.cr>
2021-06-09 18:01:48 -0600

    Inclusión de la librería JSON en maven y corrección en el nombre del proyecto

--- CINE-FRONTEND/src/main/java/services/MoviesListService.java
+++ CINE-FRONTEND/src/main/java/services/MoviesListService.java
@@ -1,20 +1,37 @@
 package services;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @WebServlet(name = "MoviesListService", urlPatterns = {"/MoviesListService"})
 public class MoviesListService extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
         
-        
+        Object[][] datos = null; // Falta retornar datos
+        try (PrintWriter out = response.getWriter())
+        {
+            JSONObject r = new JSONObject();
+            JSONArray a = new JSONArray();
+            for(Object[] registroPelicula : datos)
+            {
+                JSONObject e = new JSONObject();
+                e.put("secuencia", registroPelicula[0]);
+                e.put("nombre", registroPelicula[1]);
+                a.put(e);
+            }
+            r.put("datos_peliculas", a);
+            out.println(r);
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
