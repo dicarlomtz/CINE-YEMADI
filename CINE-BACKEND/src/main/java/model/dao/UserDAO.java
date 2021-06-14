fecc4620356e153dc64d949552b63540a1c99ba8
Yeikol Villalobos <yeikolvillalobos27@gmail.com>
2021-06-14 16:32:40 -0600

    Trabaando en login

--- CINE-BACKEND/src/main/java/model/dao/UserDAO.java
+++ CINE-BACKEND/src/main/java/model/dao/UserDAO.java
@@ -4,6 +4,7 @@ import cr.ac.una.db.Database;
 import cr.ac.una.db.dao.AbstractDAO;
 import cr.ac.una.db.dao.crud.AbstractCRUD;
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,6 +19,27 @@ public class UserDAO extends AbstractDAO<String, User> {
 
     public UserDAO() throws IOException {
         this(new DaoDB(), new UserCRUD());
+    }
+    
+    public User loginUser(String usuario, String clave) {
+        boolean found = false;
+        User user;
+        try (Connection cnx = db.getConnection()) {
+            UserCRUD ucrud = (UserCRUD) crud;
+            try (PreparedStatement stm = cnx.prepareStatement(ucrud.getRetrieveCmd())) {
+                stm.clearParameters();
+                stm.setString(1, usuario);
+                stm.setString(2, clave);
+                ResultSet rs = stm.executeQuery();
+                found = rs.next();
+                user = (User)rs;
+                return user;
+            }
+
+        } catch (SQLException ex) {
+            System.err.printf("Excepción: '%s'%n", ex.getMessage());
+        }
+        return null;
     }
 
     @Override
--- DBConnection/src/main/java/cr/ac/una/db/dao/AbstractDAO.java
+++ DBConnection/src/main/java/cr/ac/una/db/dao/AbstractDAO.java
@@ -98,5 +98,5 @@ public abstract class AbstractDAO<K, V> implements DAO<K, V> {
     }
 
     protected final Database db;
-    private final AbstractCRUD crud;
+    public final AbstractCRUD crud;
 }
