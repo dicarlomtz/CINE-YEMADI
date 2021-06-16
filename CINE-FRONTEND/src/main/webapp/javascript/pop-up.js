cd8eb75e35fb8cd9996fe78f6da51468be83256a
Yeikol Villalobos <yeikolvillalobos27@gmail.com>
2021-06-16 12:36:44 -0600

    trabajando modal popup

--- CINE-FRONTEND/src/main/webapp/javascript/pop-up.js
+++ CINE-FRONTEND/src/main/webapp/javascript/pop-up.js
@@ -1,11 +1,5 @@
-function ventanaSecundaria (){ 
-    window.open("eje.html","ventana1","width=500,height=500,scrollbars=NO") 
- } 
- 
- 
-function inite() {
-   
 
+function inite() {
     fetch('LoginService').then(function (resultado) {
         return resultado.json();
     }).then(function (datos) {
@@ -14,3 +8,49 @@ function inite() {
 
     console.log("Aplicación inicializada..");
 }
+/**CONTROL DE ASIENROS***/
+
+ const container = document.querySelector('.container');
+ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+ const count = document.getElementById('count');
+ const total = document.getElementById('total');
+ const movieSelect = document.getElementById('movie');
+ 
+ let ticketPrice = +movieSelect.value;
+ 
+ //Update total and count
+ function updateSelectedCount() {
+   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+   const selectedSeatsCount = selectedSeats.length;
+   count.innerText = selectedSeatsCount;
+   total.innerText = selectedSeatsCount * ticketPrice;
+ }
+ 
+ //Movie Select Event
+ movieSelect.addEventListener('change', e => {
+   ticketPrice = +e.target.value;
+   updateSelectedCount();
+ });
+ 
+ //Seat click event
+ container.addEventListener('click', e => {
+   if (e.target.classList.contains('seat') &&
+      !e.target.classList.contains('occupied')) {
+     e.target.classList.toggle('selected');
+   }
+   updateSelectedCount();
+ });
+    
+/**MODAL ASIENTOS***/
+function seatSelector(){
+var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+function closeSeatSelector(){
+     // When the user clicks on <span> (x), close the modal
+        var span = document.getElementsByClassName("close")[0];
+        var modal = document.getElementById("myModal");
+        span.onclick = function() {
+          modal.style.display = "none";
+        };
+} 
